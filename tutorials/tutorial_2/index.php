ec9ab1b1e9523dd4760236f3c26b653a1e8252b4
zawmyokhant <zawmyokhant429@gmail.com>
2022-02-16 15:29:01 +0630

    [master] finished tutorial_1,tutorial_2 and review update

--- tutorials/tutorial_2/index.php
+++ tutorials/tutorial_2/index.php
@@ -1,23 +1,40 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<style type="text/css">
+      body{
+            display: flex;
+            justify-content:center;
+      }
+</style>
+<body>
 <?php
 $a = 6;
-for ($b=1;$b<=$a;$b++) {
-for ($c=1;$c<=(2*$a)-1;$c++){
-    if ($c>=$a-($b-1) && $c<=$a+($b-1)) {
-        echo "*";
-    }else {
-        echo "&nbsp;&nbsp;";
-    }
-}
-echo "<br/>";
-}
-for ($b=$a-1;$b>=1;$b--) {
-    for ($c=1;$c<=(2*$a)-1;$c++){
-        if ($c>=$a-($b-1) && $c<=$a+($b-1)) {
+for ($b = 1; $b <= $a; $b++) {
+    for ($c = 1; $c <= (2 * $a) - 1; $c++) {
+        if ($c >= $a - ($b - 1) && $c <= $a + ($b - 1)) {
             echo "*";
-        }else {
+        } else {
             echo "&nbsp;&nbsp;";
         }
     }
     echo "<br/>";
+}
+for ($b = $a - 1; $b >= 1; $b--) {
+    for ($c = 1; $c <= (2 * $a) - 1; $c++) {
+        if ($c >= $a - ($b - 1) && $c <= $a + ($b - 1)) {
+            echo "*";
+        } else {
+            echo "&nbsp;&nbsp;";
+        }
     }
+    echo "<br/>";
+}
 ?>
+</body>
+</html>
